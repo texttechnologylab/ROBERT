a894018bd895418aff7e6973b76e94ba34cb4647
Kevin Bönisch <keboen@web.de>
2023-05-09 15:11:01 +0200

    Added alpaca robert dataset

--- src/training/export_dataset.py
+++ src/training/export_dataset.py
@@ -4,6 +4,16 @@ import json
 db = db()
 
 
+def get_alpaca_datasets():
+    datasets = []
+    '''used to read in the 52k datasets used for alpaca'''
+    with open('data_52k_alpaca.json', 'r') as f:
+        datas = json.load(f)
+        for data in datas:
+            datasets.append(data)
+    return datasets
+
+
 if __name__ == "__main__":
     try:
         print("Initing db...")
@@ -16,7 +26,7 @@ if __name__ == "__main__":
                 'input': data['input'],
                 'output': data['output'],
             })
-        with open('data_10k.json', 'w') as f:
+        with open('data_62k_alpaca_robert.json', 'w') as f:
             json.dump(datasets, f)
         print("Done exporting.")
     except Exception as e:
