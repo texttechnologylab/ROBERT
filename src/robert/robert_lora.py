f66f1d5d11e3da4c1c9da25c2af8ee4f10fcba99
Kevin Bönisch <keboen@web.de>
2023-06-19 08:39:17 +0200

    prompting

--- src/robert/robert_lora.py
+++ src/robert/robert_lora.py
@@ -82,7 +82,7 @@ class robert:
         if message["input"]:
             return (
                 "Below is an instruction that describes a task, paired with an input that provides further context. "
-                "Write a response as Rob that appropriately continues the inputs.\n\n"
+                "Write a response that appropriately continues the inputs.\n\n"
                 f"### Instruction:\n{message['instruction']}\n\n### Input:\nDialog so far:\n{message['input']}\n\n### Response:"
             )
         return (
@@ -95,7 +95,7 @@ class robert:
         '''Takes in a prompt and returns and answer from robert'''
         # We use the optional input field to store the existing chat
         # and context. We take the last X entries to the context.
-        inp = '\n'.join(self.context[-4:])
+        inp = '\n'.join(self.context[-2:])
         sample = {"instruction": message, "input": inp}
         prompt = self.generate_prompt(sample)
         #print("===================== PROMPT =====================")
